bf7acef4c1cda780241476385b812014f2eb8f5d
Eindride <claudiu.ncl97@gmail.com>
2018-09-03 15:52:21 +0300

    Integrated with hotjar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { hotjar } from 'react-hotjar';
 import { firebase } from './firebase/index';
 
 import Main from './components/Main/Main';
@@ -8,11 +9,14 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 import { setLoggedInUser } from './redux/actions';
+import { hjid, hjsv } from './hotjar-config';
 
 import './App.scss';
 /* eslint-disable */
 class App extends Component {
   componentDidMount() {
+    hotjar.initialize(hjid, hjsv);
+
     const { setLoggedInUser } = this.props;
     firebase.auth.onAuthStateChanged(authUser => {
       authUser
